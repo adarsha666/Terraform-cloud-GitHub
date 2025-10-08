1863deb0181b18b5fe0f16d1a7a113f0af6ebd1b
adarsha666 <adarshangadi79@gmail.com>
2025-10-08 19:02:03 +0530

    Updated main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,23 @@
 terraform {
-    required_version = "~> 1.13.3" // added latest version
-    required_providers {
+  required_version = "~> 1.13.3" //  Value mention as per your terraform version
+  required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.15.0"
-     }
+      version = "~> 6.15.0" // Value mention as per your aws version
     }
-   }
+  }
+}
 
 provider "aws" {
-        region = "us-east-1" 
-        #profile = "default"
-	}
+  region = "us-east-1"
 
-resource "aws_instance" "Devops" {
-	ami = "ami-052064a798f08f0d3" 
-	instance_type = "t2.micro"
+}
+
+resource "aws_instance" "noname" {
+  ami           = "ami-052064a798f08f0d3" // Refer aws and get correct ami id
+  count = 3
+  instance_type = "t2.micro"
+  tags = {
+        	Name = "Terraforminst-${count.index}"
+ 	}
 }
